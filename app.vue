047649372f012afee34e159eb197b6e83a2f1ec9
Ceci Olivera <ceci.coding@gmail.com>
2026-04-30 08:58:37 +0200

    redirect to browser lang and other copy changes

--- app.vue
+++ app.vue
@@ -3,3 +3,21 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const { locale } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+const route = useRoute()
+
+onMounted(() => {
+  const browserLang = navigator.language
+  const hasLangParam = route.query.lang !== undefined
+  
+  if (browserLang.startsWith('en') && !hasLangParam && locale.value === 'es') {
+    const newPath = switchLocalePath('en')
+    if (newPath && window.location.pathname !== newPath) {
+      window.location.href = newPath
+    }
+  }
+})
+</script>
